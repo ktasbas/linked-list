6ad4c332e9e3213638a25b111f307a61c64bd558
M. Kaan Tasbas <mktasbas@gmail.com>
2018-11-10 01:38:29 -0500

    finished destructor and tested

--- main.cpp
+++ main.cpp
@@ -4,3 +4,57 @@
 /**
  * Simple linked list class to practice various coding concepts
  **/
+
+struct Node{
+	int data;
+	Node* next;
+};
+
+class List{
+	public:
+	
+	// constructor
+	List(){
+		this->head = nullptr;
+	}
+
+	// destructor
+	~List(){
+		//check if list is empty
+		if(this->isEmpty()) return;
+		// begin at the head of list
+		Node* current = this->head;
+		// iterate list
+		while(current->next != nullptr)
+		{
+			// save pointer to next node
+			Node* next = current->next;
+			// delete current node
+			delete current;
+			// move to next node
+			current = next;
+		}
+		// now current = last node
+		// delete final node
+		delete current;
+	}
+
+	void addNode(Node* node){
+
+	}
+
+	bool isEmpty(void){
+		if (this->head == nullptr) return true;
+		else return false;
+	}
+
+	private:
+
+	Node* head;
+};
+
+int main()
+{
+	List list;
+	return 0;	
+}
